3ef0642e225906d7a7e0d50eceead357d27fd47f
bpastorelli <papitopastorelli@gmail.com>
2019-10-01 10:07:26 -0300

    Criadas novos objetos

---
+++ src/test/java/br/com/mobile/regression/BaseTestCase.java
@@ -0,0 +1,34 @@
+package br.com.mobile.regression;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+
+import br.com.mobile.commons.Property;
+import br.com.mobile.commons.SetupEnviroment;
+
+
+public class BaseTestCase {
+	
+	public static WebDriver driver;
+	
+	public static SetupEnviroment setup = new SetupEnviroment();
+	
+	@BeforeClass
+	public static void beforeClass() throws IOException, URISyntaxException {
+		
+		Property.loadProperties();
+		setup.setupEnviroment();
+	}
+	
+	@AfterClass
+	public static void afterClass() throws IOException {
+		
+		//LogReport.finalizarReport();
+		setup.driverClose();
+		setup.serviceStop();
+	}
+}
---
+++ src/test/java/br/com/mobile/tests/MobileTests.java
@@ -0,0 +1,24 @@
+package br.com.mobile.tests;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+
+import br.com.mobile.regression.AppConfiguration;
+import br.com.mobile.regression.BaseTestCase;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources/features/Mobile.feature"}, 
+glue = { "br.com.b3.sinacor.stepDefinition" }, 
+plugin = {"html:target/cucumber-html-report","json:target/cucumber.json"},
+format = {"pretty", "html:target/reports/cucumber/html",
+        "json:target/cucumber.json", "usage:target/usage.jsonx", "junit:target/junit.xml"}
+		,tags = {"@winium"})
+
+@ContextConfiguration(classes= AppConfiguration.class)
+public class MobileTests extends BaseTestCase {
+	
+	
+
+}
