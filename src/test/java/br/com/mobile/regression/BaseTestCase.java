caa59d83970e95ed514fb950f79aa73ea35a190a
bpastorelli <papitopastorelli@gmail.com>
2019-10-08 14:48:32 -0300

    Modificações

--- src/test/java/br/com/mobile/regression/BaseTestCase.java
+++ src/test/java/br/com/mobile/regression/BaseTestCase.java
@@ -2,7 +2,6 @@ package br.com.mobile.regression;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
-import org.openqa.selenium.WebDriver;
 
 import br.com.mobile.commons.Property;
 import br.com.mobile.config.ConfigPages;
@@ -10,8 +9,6 @@ import br.com.mobile.implementations.SetupAndroid;
 import br.com.mobile.reports.LogReport;
 
 public class BaseTestCase {
-	
-	public static WebDriver driver;
 	
 	public static SetupAndroid setup = new SetupAndroid();
 	
