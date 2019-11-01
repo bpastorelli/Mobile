a05d78e67485923e0b9102e95d2ac6a006ae1589
bpastorelli <papitopastorelli@gmail.com>
2019-11-01 15:13:36 -0300

    Sleep da thread

--- src/test/java/br/com/mobile/utils/Threads.java
+++ src/test/java/br/com/mobile/utils/Threads.java
@@ -13,6 +13,11 @@ public class Threads {
 				
 				cmd.executeCommand("appium -a 0.0.0.0 -p 4723");
 				Utils.log("Appium iniciado via Thread...");
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}.start();
 		
