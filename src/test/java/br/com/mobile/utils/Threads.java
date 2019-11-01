2971762ea6af72c331993b8c01040ce928f7f60f
bpastorelli <papitopastorelli@gmail.com>
2019-11-01 16:53:42 -0300

    Teses

--- src/test/java/br/com/mobile/utils/Threads.java
+++ src/test/java/br/com/mobile/utils/Threads.java
@@ -1,25 +1,48 @@
 package br.com.mobile.utils;
 
+import java.io.IOException;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecuteResultHandler;
+import org.apache.commons.exec.DefaultExecutor;
+
 import br.com.mobile.commons.Property;
 
 public class Threads {
 
 	Command cmd = new Command();
-	
+
 	public void startAppium() {
-		
-		new Thread() {
-			
-			@Override
-			public void run() {
-				
+
+		//new Thread() {
+
+			//@Override
+			//public void run() {
+
 				cmd.executeCommand("netstat -ano | findstr " + Property.APPIUM_PORT);
 				cmd.killProcessPort();
-				cmd.executeCommand("npm install && appium -a " + Property.APPIUM_IP + " -p " + Property.APPIUM_PORT, false);
+
+				CommandLine cmd = new CommandLine("C:\\Program Files\\nodejs\\node.exe");
+				cmd.addArgument("C:\\Users\\bodl\\AppData\\Local\\Programs\\Appium\\resources\\app\\node_modules\\appium\\build\\lib\\main.js");
+				cmd.addArgument("--address");
+				cmd.addArgument("0.0.0.0");
+				cmd.addArgument("--port");
+				cmd.addArgument("4723");
+
+				DefaultExecuteResultHandler handler = new DefaultExecuteResultHandler();
+				DefaultExecutor executor = new DefaultExecutor();
+				executor.setExitValue(1);
+				try {
+					executor.execute(cmd, handler);
+					Thread.sleep(20000);
+				} catch (IOException | InterruptedException e) {
+					e.printStackTrace();
+				}
+
 				Utils.log("[APPIUM]Appium iniciado via Thread...");
 			}
-		}.start();
-		
-	}
-	
+		//}.start();
+
+	//}
+
 }
